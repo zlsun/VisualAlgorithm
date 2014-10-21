302d5a5d92bec8997660e14259d7a248d5a9192d
zlsun <zlsun1995@gmail.com>
2014-10-21 14:59:38 +0800

    Auto clean and copy files when building

--- build.bat
+++ build.bat
@@ -1,1 +1,4 @@
-pyinstaller ./src/main_window.pyw -i ./src/icons/icon.ico --onefile --noconsole
+rd /S /Q dist
+pyinstaller src\main_window.pyw -i src\icons\icon.ico --onefile --noconsole
+mklink /J dist\examples\ src\examples\
+mklink /J dist\icons\    src\icons\
